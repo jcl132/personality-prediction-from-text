255cc13e3250095aeefe99deda9eaee1bd85f21d
Jason Li <jcl132@gmail.com>
2018-05-09 17:43:38 -0700

    tuned some hyperparams, messed around with regression model

---
+++ data_prep.py
@@ -0,0 +1,129 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import MinMaxScaler
+
+class DataPrep():
+    def __init__(self):
+        self.trait_dict = {
+            'O': 'cOPN',
+            'C': 'cCON',
+            'E': 'cEXT',
+            'A': 'cAGR',
+            'N': 'cNEU',
+            'Openness': 'cOPN',
+            'Conscientiousness': 'cCON',
+            'Extraversion': 'cEXT',
+            'Agreeableness': 'cAGR',
+            'Neuroticism': 'cNEU'
+            }
+        self.trait_score_dict = {
+            'O': 'sOPN',
+            'C': 'sCON',
+            'E': 'sEXT',
+            'A': 'sAGR',
+            'N': 'sNEU',
+            'Openness': 'sOPN',
+            'Conscientiousness': 'sCON',
+            'Extraversion': 'sEXT',
+            'Agreeableness': 'sAGR',
+            'Neuroticism': 'sNEU'
+            }
+        self.LIWC_features = [
+            'WPS', 'Unique', 'Dic', 'Sixltr', 'Negate', 'Assent', 'Article', 'Preps', 'Number',
+            'Pronoun', 'I', 'We', 'Self', 'You', 'Other',
+            'Affect', 'Posemo', 'Posfeel', 'Optim', 'Negemo', 'Anx', 'Anger', 'Sad',
+            'Cogmech', 'Cause', 'Insight', 'Discrep', 'Inhib', 'Tentat', 'Certain',
+            'Senses', 'See', 'Hear', 'Feel',
+            'Social', 'Comm', 'Othref', 'Friends', 'Family', 'Humans',
+            'Time', 'Past', 'Present', 'Future',
+            'Space', 'Up', 'Down', 'Incl', 'Excl', 'Motion',
+            'Occup', 'School', 'Job', 'Achieve',
+            'Leisure', 'Home', 'Sports', 'TV', 'Music',
+            'Money',
+            'Metaph', 'Relig', 'Death', 'Physcal', 'Body', 'Sexual', 'Eating', 'Sleep', 'Groom',
+            'Allpct', 'Period', 'Comma', 'Colon', 'Semic', 'Qmark', 'Exclam', 'Dash', 'Quote', 'Apostro', 'Parenth', 'Otherp',
+            'Swear', 'Nonfl', 'Fillers',
+        ]
+
+    def prep_data(self, type, trait, regression=False):
+        df_status = self.prep_status_data()
+        df_essay = self.prep_essay_data()
+
+        tfidf = TfidfVectorizer(stop_words='english', strip_accents='ascii')
+
+        if type == 'essay':
+
+            result = tfidf.fit_transform(df_essay['TEXT']).todense()
+
+            scaler = MinMaxScaler()
+            other_features_df = scaler.fit(df_essay[self.LIWC_features])
+
+            X = np.nan_to_num(np.column_stack((result, )))
+
+            # X = tfidf.fit_transform(df_essay['TEXT'])
+            y_column = self.trait_dict[trait]
+            y = df_essay[y_column]
+
+        elif type == 'status':
+
+
+            # Include other features with tfidf vector
+            other_features_columns = [
+                'NETWORKSIZE',
+                'BETWEENNESS',
+                'NBETWEENNESS',
+                'DENSITY',
+                'BROKERAGE',
+                'NBROKERAGE',
+                'TRANSITIVITY'
+            ]
+            result = tfidf.fit_transform(df_status['STATUS']).todense()
+            X = np.nan_to_num(np.column_stack((result, df_status[other_features_columns])))
+
+
+            # straight up tfidf vector
+            # X = tfidf.fit_transform(df_status['STATUS'])
+            if regression:
+                y_column = self.trait_score_dict[trait]
+            else:
+                y_column = self.trait_dict[trait]
+            y = df_status[y_column]
+
+        return X, y
+
+
+    def prep_status_data(self):
+        df = pd.read_csv('data/myPersonality/mypersonality_final.csv', encoding="ISO-8859-1")
+        df = self.convert_traits_to_boolean(df)
+        return df
+
+
+    def prep_essay_data(self):
+        df_essays = pd.read_csv('data/personality-detection-my-copy/essays.csv', encoding="ISO-8859-1")
+        df_mairesse = pd.read_csv('data/personality-detection-my-copy/mairesse.csv', encoding="ISO-8859-1", header=None)
+
+
+        df_mairesse.columns = ['#AUTHID'] + self.LIWC_features
+
+        df = df_essays.merge(df_mairesse, how = 'inner', on = ['#AUTHID'])
+
+        # add word count (WC) column
+        df['WC'] = df['TEXT'].str.split().str.len()
+
+        df = self.convert_traits_to_boolean(df)
+
+        return df
+
+    def convert_traits_to_boolean(self, df):
+        trait_columns = ['cOPN', 'cCON', 'cEXT', 'cAGR', 'cNEU']
+        d = {'y': True, 'n': False}
+
+        for trait in trait_columns:
+            df[trait] = df[trait].map(d)
+
+        return df
+
+
+    def load_data(self, filepath):
+        return pd.read_csv(filepath, encoding="ISO-8859-1")
---
+++ model_eval.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier, GradientBoostingClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression, LinearRegression, Ridge, SGDRegressor
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import cross_validate
+from sklearn.metrics import f1_score, mean_squared_error
+
+class ModelEvaluator():
+    def __init__(self, X, y, trait):
+        self.X = X
+        self.y = y
+        self.trait = trait
+        self.models_dict = {
+            'LogisticRegression': LogisticRegression(),
+            'RandomForestClassifier': RandomForestClassifier(max_features='sqrt', n_estimators=110),
+            'MultinomialNB': MultinomialNB(),
+            'GradientBoostingClassifier': GradientBoostingClassifier(),
+            'SVC': SVC(),
+            'LinearRegression': LinearRegression(),
+            'RandomForestRegressor' : RandomForestRegressor(),
+            'Ridge': Ridge(),
+            'SGDRegressor': SGDRegressor(),
+        }
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.33, random_state=32)
+        self.hyperparameters = {
+        'RandomForestClassifier': {'max_features': 'sqrt', 'n_estimators': 110},
+
+        }
+
+    # def tune_hyperparameters(self):
+    #     for model_name, model in self.models_dict.items():
+    #
+    #         param_grid = {
+    #             'n_estimators': [50, 100, 150, 200],
+    #             'max_features': ['auto', 'sqrt', 'log2']
+    #         }
+    #
+    #         CV = GridSearchCV(estimator=model, param_grid=param_grid, cv= 5)
+    #         CV.fit(self.X, self.y)
+    #         CV.best_params_)
+
+    def compare_scores(self, models, regression=False):
+        print('Model performance for trait ' + self.trait + ' prediction:' + '\n')
+
+        accuracy_scores = []
+        f1_scores = []
+
+        for model_name in models:
+            model = self.models_dict[model_name]
+            model.fit(self.X_train, self.y_train)
+
+            print(model_name + ": ")
+
+            if regression:
+                y_pred = model.predict(self.X_test)
+                y_true = self.y_test
+                mse = -np.mean(cross_validate(model, self.X_test, self.y_test, scoring='neg_mean_squared_error', cv=10)['test_score'])
+                print('MSE: ' + str(mse) + '\n')
+            else:
+                accuracy_score = np.mean(cross_validate(model, self.X_test, self.y_test, cv=10)['test_score'])
+                accuracy_scores.append(accuracy_score)
+                print('Accuracy score: ' + str(accuracy_score) + '\n')
+
+                # y_pred = model.predict(self.X_test).round()
+                # y_true = self.y_test
+                # f_score = f1_score(y_true, y_pred)
+                f_score = np.mean(cross_validate(model, self.X_test, self.y_test, scoring='f1', cv=10)['test_score'])
+                f1_scores.append(f_score)
+                print('F1 score: ' + str(f_score) + '\n')
+
+        if regression:
+            pass
+        else:
+            best_accuracy_score = max(accuracy_scores)
+            best_accuracy_model = models[accuracy_scores.index(best_accuracy_score)]
+            print(
+                'Best Accuracy score: ' + str(best_accuracy_score) + '\n' +
+                'Model: ' + best_accuracy_model + '\n' + '\n'
+            )
+            best_f1_score = max(f1_scores)
+            best_f1_model = models[f1_scores.index(best_f1_score)]
+            print(
+                'Best F1 score: ' + str(best_f1_score) + '\n' +
+                'Model: ' + best_f1_model + '\n'
+            )
